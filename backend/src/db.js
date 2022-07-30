ff7ebbfea5bdf3f3081aad02133c438c0ad6beed
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-07-30 15:55:54 -0400

    connect Mongo Atlas

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,15 +1,16 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017";
 
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
+    client = await MongoClient.connect(dbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-}
+};
 
-export const getDbConnection = dbName => {
+export const getDbConnection = (dbName) => {
     const db = client.db(dbName);
     return db;
-}
+};
