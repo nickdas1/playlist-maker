c205a38cf8800fbd22fbf84a23490a6b86b80c14
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-08-18 10:53:32 -0400

    fix oauth callback url

--- backend/src/util/oauthClient.js
+++ backend/src/util/oauthClient.js
@@ -3,5 +3,5 @@ const { google } = require("googleapis");
 module.exports.oauthClient = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    process.env.PROD_URI || "http://localhost:8080" + "/auth/google/callback"
+    (process.env.PROD_URI || "http://localhost:8080") + "/auth/google/callback"
 );
